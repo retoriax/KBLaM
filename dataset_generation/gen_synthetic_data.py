d7c0e54e67c5b757a20747cd981ae73263956450
Liana Mikaelyan <45925959+LianaMikael@users.noreply.github.com>
2024-11-19 23:50:35 +0000

    Add names perturbation in queries (#9)
    * Add names perturbation in queries
    
    ---------
    
    Co-authored-by: Liana Mikaelyan <lmikaelyan@microsoft.com>

--- dataset_generation/gen_synthetic_data.py
+++ dataset_generation/gen_synthetic_data.py
@@ -227,6 +227,7 @@ class SyntheticDataGenerator(GPT):
             try:
                 prompt = "Generate an extended Q and an A for this pair: " + f"Q: {data.Q}\nA: {data.A}"
                 gpt_output = self.generate_response(prompt)
+                print(gpt_output)
                 extended_q = re.findall(r"Q: (.*)", gpt_output)[0]
                 extended_a = re.findall(r"A: (.*)", gpt_output)[0]
                 data.extended_Q = extended_q
@@ -236,6 +237,23 @@ class SyntheticDataGenerator(GPT):
                 print(e)
                 continue
         return dataset
+    
+
+    def perturb_names(self, dataset: list[DataPoint]):
+        for data in dataset:
+            try:
+                prompt = f"Perturb the names in the queries of the dataset (e.g. Margaret Thatcher -> Maggie Thatcher or Microsoft Research to MSR) for data point with name {data.name}."
+                prompt += f"Return the question {data.Q} with the perturbed name. Make sure the perturbation is valid. Do NOT generate anything else."
+                gpt_output = self.generate_response(prompt)
+                data.Q = gpt_output
+
+            except Exception as e:
+                print(f"Error perturbing the names in the queries.")
+                print(e)
+                continue
+        return dataset
+
+
 
 
 def parser_args():
@@ -246,6 +264,7 @@ def parser_args():
     parser.add_argument("--generate_related_people", type=bool, default=True)
     parser.add_argument("--raw_output_file", type=str, default="synthetic_data_raw.json")
     parser.add_argument("--output_file", type=str, default="synthetic_data_QA.json")
+    parser.add_argument("--perturbed_output_file", type=str, default="perturbed_output_file")
     parser.add_argument("--augmented_output_file", type=str, default="synthetic_data_QA_augmented.json")
 
     args = parser.parse_args()
@@ -259,33 +278,52 @@ if __name__ == "__main__":
 
     os.makedirs(args.output_path, exist_ok=True)
 
-    entity_list = []
-    for seed in range(1):
-        data_generator.set_seed(seed)
-        for instruction in tqdm(data_generator.get_instructions()):
-            try:
-                entity = data_generator.generate_entity(instruction)
-            except Exception as e:
-                print(f"Error generating entity.")
-                print(e)
-                continue
-            save_entity(entity, os.path.join(args.output_path, args.raw_output_file))
-            entity_list.append(entity)
+    raw_output_file = os.path.join(args.output_path, args.raw_output_file)
 
-            if args.generate_related_people:
+    if os.path.exists(raw_output_file):
+        # skip entities creation if it's already generated
+        with open(raw_output_file, "r") as file:
+            entity_list = [Entity(**json.loads(line)) for line in file]
+
+    else:
+        entity_list = []
+        for seed in range(1):
+            data_generator.set_seed(seed)
+            for instruction in tqdm(data_generator.get_instructions()):
                 try:
-                    response = data_generator.generate_related_data(entity)
+                    entity = data_generator.generate_entity(instruction)
                 except Exception as e:
                     print(f"Error generating entity.")
                     print(e)
                     continue
-                save_entity(response, os.path.join(args.output_path, args.raw_output_file))
-                entity_list.append(response)
+                save_entity(entity, raw_output_file)
+                entity_list.append(entity)
 
-    dataset = data_generator.post_process_data(entity_list)
-    for data in dataset:
-        save_entity(data, os.join(args.output_path, args.output_file))
+                if args.generate_related_people:
+                    try:
+                        response = data_generator.generate_related_data(entity)
+                    except Exception as e:
+                        print(f"Error generating entity.")
+                        print(e)
+                        continue
+                    save_entity(response, raw_output_file)
+                    entity_list.append(response)
+
+    QA_output_file = os.path.join(args.output_path, args.output_file)
+
+    if os.path.exists(QA_output_file):
+        with open(QA_output_file, "r") as file:
+            dataset = [DataPoint(**json.loads(line)) for line in file]
+    else:
+        dataset = data_generator.post_process_data(entity_list)
+        for data in dataset:
+            save_entity(data, QA_output_file)
+
+    perturbed_dataset = data_generator.perturbe_names(dataset)
+
+    for data in perturbed_dataset:
+        save_entity(data, os.path.join(args.output_path, args.perturbed_output_file))
 
     dataset = data_generator.augmenta_data_with_synthetic_QA(dataset)
     for data in dataset:
-        save_entity(data, os.join(args.output_path, args.augmented_output_file))
+        save_entity(data, os.path.join(args.output_path, args.augmented_output_file))
